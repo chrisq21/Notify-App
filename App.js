f26b5ba29356136c197a425c9c2149eae56a8534
Chris Queen <chrisqueen10@gmail.com>
2019-12-22 16:45:05 -0500

    Moved home component into it's own folder

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 
-import HomeScreen from './components/Home.js'
+import HomeScreen from './components/Home/index.js'
 
 const MainNavigator = createStackNavigator({
   Home: { screen: HomeScreen }
--- components/Home/index.js
+++ components/Home/index.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 function Home() {
   return (
     <View>
-      <Text>Hello World</Text>
+      <Text>Hello World in folder </Text>
     </View>
   )
 }
