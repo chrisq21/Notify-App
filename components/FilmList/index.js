f8284049a7fed23ab48172c41c050ee833b880cf
Chris Queen <chrisqueen10@gmail.com>
2019-12-24 11:12:31 -0500

    Changed FilmTimer component to class based component. Added proof-of-concept implementation of timer feature

--- components/FilmList/index.js
+++ components/FilmList/index.js
@@ -4,11 +4,11 @@ import FilmListing from "./FilmListing/index.js";
 
 function FilmList({ navigate }) {
   const mockFilmData = [
-    { title: "Star Wars", id: "1" },
-    { title: "Get Him to the Greek", id: "2" },
-    { title: "The Big Sleep", id: "3" },
-    { title: "The Big Lebowski", id: "4" },
-    { title: "Inception", id: "5" }
+    { title: "Star Wars", id: "1", totalSeconds: 3600 },
+    { title: "Get Him to the Greek", id: "2", totalSeconds: 3600 },
+    { title: "The Big Sleep", id: "3", totalSeconds: 3600 },
+    { title: "The Big Lebowski", id: "4", totalSeconds: 60 },
+    { title: "Inception", id: "5", totalSeconds: 30 }
   ];
   return (
     <View>
--- components/FilmTimer/index.js
+++ components/FilmTimer/index.js
@@ -1,13 +1,67 @@
 import React from "react";
-import { SafeAreaView, Text, Button } from "react-native";
+import { ProgressViewIOS, View, Button, Text, Vibration } from "react-native";
 
-function FilmTimer({ navigation }) {
-  const title = JSON.stringify(navigation.getParam("title", ""));
-  return (
-    <SafeAreaView>
-      <Text>{title}</Text>
-    </SafeAreaView>
-  );
+class FilmTimer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filmDuration: 0.0,
+      demoTargetText: "notTargetTime",
+      didUnmount: false
+    };
+
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+  }
+
+  componentWillUnmount() {
+    this.setState({ didUnmount: true });
+    this.stopTimer();
+  }
+
+  componentDidUpdate() {
+    const { filmDuration, demoTargetText } = this.state;
+    const targetTime = 15;
+    if (
+      filmDuration >= targetTime / 100 &&
+      demoTargetText !== "reached target time"
+    ) {
+      this.setState({ demoTargetText: "reached target time" });
+    }
+  }
+
+  startTimer() {
+    const { navigation } = this.props;
+    const { didUnmount } = this.state;
+    const totalSeconds = JSON.stringify(
+      navigation.getParam("totalSeconds", 0.0)
+    );
+    const incrementer = 1 / totalSeconds;
+    this.interval = setInterval(() => {
+      if (!didUnmount) {
+        this.setState({ filmDuration: this.state.filmDuration + incrementer });
+      }
+    }, 1000);
+  }
+
+  stopTimer() {
+    if (this.interval) clearInterval(this.interval);
+  }
+
+  render() {
+    const { demoTargetText, filmDuration } = this.state;
+    const { navigation } = this.props;
+    const title = JSON.stringify(navigation.getParam("title", ""));
+    return (
+      <View>
+        <Text>{demoTargetText}</Text>
+        <Button title="Start" onPress={this.startTimer} />
+        <Button title="Stop" onPress={this.stopTimer} />
+        <ProgressViewIOS progress={this.state.filmDuration} />
+      </View>
+    );
+  }
 }
 
 export default FilmTimer;
